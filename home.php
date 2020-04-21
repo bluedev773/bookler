2d48e9612516634a3da7485e709afd0a70fea299
Matthew Litvinov <litvinma773@hotmail.com>
2020-04-20 20:10:23 -0400

    grid links added

--- home.php
+++ home.php
@@ -15,18 +15,49 @@ if (!isset($_SESSION['loggedin'])) {
 		<title>Home Page</title>
 		<link href="styles/style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		
 	</head>
-	<body class="loggedin">
+	<body class="loggedin" onload="fetchBookmarks()">
 		<nav class="navtop">
 			<div>
-				<h1>Bookmark Manager</h1>
+				<a href="home.php" id="logo">
+                    <img src="images/logo.png" alt="Bookler! an intuitive bookmark manager." height='75' width='75'>
+                </a>
+				<h1>Bookler</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
+			<hr>
 		</nav>
-		<div class="content">
-			<h2>Home Page</h2>
+		<div class="content" id="topTen">
+			<h2>My Top Ten</h2>
 			<p>Welcome back, <?=$_SESSION['name']?>!</p>
 		</div>
+<!-- TODO: link inputs to database -->
+		<div class="content" id="add">
+			<h2>Add Bookmarks</h2>
+			<form id = "addForm">
+				<div class = "form-group">
+					<label>Site Name</label>
+					<input type="text" class = "form-control" id = "siteName" placeholder = "Website Name">
+				</div>
+				<div class = "form-group">
+					<label>Site URL</label>
+					<input type="text" class = "form-control" id = "siteUrl" placeholder = "Website URL">
+				</div>
+				<button type = "submit">Submit</button>
+			</form>
+		</div>
+		<div class="content" >
+			<h2>All Bookmarks</h2>
+			
+		</div>
+	
+				<div class = "wrapper" id="bookmarksResults">	
+				</div>
+			
+		
+
+		<script src="scripts/add.js"></script>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -4,21 +4,23 @@
         <meta charset="utf-8">
         <title>Home Page</title>
         <link rel="stylesheet" href="styles/style.css" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
     </head>
-    <body>
+    <body class = "index">
         <nav class = "navtop">
             <div>
-                <a href="index.html">
+                <a href="index.php"  id="logo">
                     <img src="images/logo.png" alt="Bookler! an intuitive bookmark manager." height='75' width='75'>
                 </a>
                 <h1>Bookler</h1>
-                <a href="login.html">Login</a>
-				<a href="register.html">Register</a>
+                <a href="login.html"><i class="fas fa-sign-in-alt"></i>Login</a>
+				<a href="register.html"><i class="fas fa-user-plus"></i></i>Register</a>
             </div>
             <hr>
         </nav>
         <div class="content">
-            <h1>Home Page</h1>
+            <h2>Welcome to Bookler, an intuitive bookmark manager! Login or make an account to get started.</h2>
+            <p>test</p>
         </div>
     </body>
 </html>
