2ac4afbfbb2ecbeb5801897b5ea894c76cd6c0f0
Matthew Litvinov <litvinma773@hotmail.com>
2020-04-25 13:32:35 -0400

    top bookmarks

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<!DOCTYPE html>
+<?php
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'phplogin';
+$conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+//get the bookmark info from database
+$sql = "SELECT name,url u FROM bookmarks GROUP BY name ORDER BY u asc limit 10;";
+$result = $conn->query($sql);
+
+?>
+<script>
+function addhttp(url) {
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+      url = "http://" + url;
+  }
+  return url;
+}
+</script>
+
 <html>
     <head>
         <meta charset="utf-8">
@@ -23,12 +48,20 @@
             <h1>Welcome to Bookler, an intuitive bookmark manager!</h1>
         </div>
         <!--display top ten bookmarks across all users-->
-		<div class="content" id="topTen">
-			<h2>Top Ten Bookmarked Webpages!</h2>
+		<div class="content">
+			<h2>Top Bookmarked Webpages!</h2>
         </div>
        
         <div class = "wrapper" id="topTen">	
-            <p>test</p>
+			 <?php 
+				// 
+				while($row = $result->fetch_assoc()){
+					echo '<div class = "box" onclick="window.open(addhttp(\''.$row["u"].'\'))">'.
+	                                        '<h3>'.$row["name"].'</h3>'.
+	                                        '</div>';
+				};
+			 ?> 
+
 		</div>
     </body>
 </html>
