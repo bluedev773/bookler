c259915ad4b38d39992c6aa0b69c687e470b4050
Matthew Litvinov <55808264+BlueFin773@users.noreply.github.com>
2021-08-25 16:45:09 -0400

    Update register.php

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 //connect to database
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'GwKz3CiBKVmX';
 $DATABASE_NAME = 'phplogin';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
