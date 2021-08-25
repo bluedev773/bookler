0781771e14c025872a0bcb18555cc03c9dd838e0
Matthew Litvinov <55808264+BlueFin773@users.noreply.github.com>
2021-08-25 16:41:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'GwKz3CiBKVmX';
 $DATABASE_NAME = 'phplogin';
 $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
